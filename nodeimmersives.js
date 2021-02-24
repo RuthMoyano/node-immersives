051ee12fde31a1b8424577b5301642e3bb492f88
Ruth Moyano <ruth.moyano@codeimmersives.com>
2021-02-24 09:56:22 -0600

    [Add all changes]

--- nodeimmersives.js
+++ nodeimmersives.js
@@ -1,2 +1,12 @@
 // Check the get-input.js code as well as the README for the lowdown on the beefing up we've done!
 const getInput = require("./get-input.js");
+
+const firstName = getInput(0);
+const lastName = getInput(1);
+const fullName = firstName + ' ' + lastName;
+const emailAddress = firstName.toLowerCase() + '.' + lastName.toLowerCase() + '@codeimmersives.com';
+
+console.log('Hi ' + firstName + ' ' + lastName + ', how are you?');
+console.log('Your name uppercase would be ' + fullName.toUpperCase());
+console.log('Your initials are ' + firstName[0] + '.' + lastName[0] + '.');
+console.log('Your email address for school is ' + emailAddress);
